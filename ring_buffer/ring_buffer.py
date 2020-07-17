750e13a4a9d4702ef73b1e0e4f2c6c2371579219
Jess Daniel <jessdaniel474@gmail.com>
2020-07-17 17:43:36 -0400

    final push

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -2,19 +2,21 @@ class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
         self.list = []
+        self.current = 0
         
     def append(self, item):
         # case 1: list is not full
         if len(self.list) < self.capacity:
             return self.list.append(item)
         # case 2: list is full
-        current = 0
-        while current < self.capacity:
-            self.list.pop()
-            self.list[current] = item
-            current = current + 1
+        else:
+            if self.current == self.capacity:
+                self.current = 0
+            self.list[self.current] = item
+            self.current = self.current + 1
 
     def get(self):
         if type(self.list) is not list:
             return []
         return self.list
+
