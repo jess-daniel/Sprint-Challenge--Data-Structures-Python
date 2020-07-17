4f02effc6291ab1e6fa19cbf210516a64d4a6f55
Jess Daniel <jessdaniel474@gmail.com>
2020-07-17 13:25:13 -0400

    reverse linked list passes

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,20 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
-
+        self.capacity = capacity
+        self.list = []
+        
     def append(self, item):
-        pass
+        # case 1: list is not full
+        if len(self.list) < self.capacity:
+            return self.list.append(item)
+        # case 2: list is full
+        current = 0
+        while current < self.capacity:
+            self.list.pop()
+            self.list[current] = item
+            current = current + 1
 
     def get(self):
-        pass
+        if type(self.list) is not list:
+            return []
+        return self.list
